ab4a89608b6b232947a1a553f08ff02cb41ae996
Abdelrahman Mahmoud <a.mahmoud1994@gmail.com>
2017-04-29 19:33:19 +0200

    adding the single album page

--- src/components/SingleAlbum.js
+++ src/components/SingleAlbum.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {API_URL} from "../App"
 import axios from 'axios';
+import TrackList from "./TrackList"
+import {Link} from 'react-router-dom'
 
 export default class SingleAlbum extends React.Component{
   constructor(props){
       super(props);
       this.state={
-        album: {}
+        album: {},
+        artists:[]
       }
   }
 
@@ -15,18 +18,42 @@ export default class SingleAlbum extends React.Component{
     axios.get(`${API_URL}/albums/${id}`).then(
       (response) => {
         this.setState({
-          album:response.data
+          album:response.data,artists:response.data.artists
         })
       }
     )
   }
-  render(){
 
+  render(){
+    if (!this.state.artists[0]) {
+      return null;
+    }
+    console.log(this.state.album);
     return(
         <div>
-          <h1>
-            {this.state.album.name}
-          </h1>
+
+        <img src={this.state.album.images[0].url}/>
+
+        <h1>
+          {this.state.album.name}
+        </h1>
+
+        <h1>
+          {this.state.artists[0].name}
+        </h1>
+
+        <h4>
+          {this.state.album.tracks.total} tracks
+        </h4>
+
+        <Link to={`/artist/${this.state.artists[0].id}`}>
+        <button>ARTIST PROFILE </button>
+        </Link>
+
+
+        <TrackList tracks={this.state.album.tracks.items} playTrack={this.props.playTrack}/>
+
+
         </div>
 
     )
