e08c42eafe5e081ccac3c2b013b9d1dc5f68c8e0
Abdelrahman Mahmoud <a.mahmoud1994@gmail.com>
2017-05-05 13:13:16 +0200

    Adding style to the player except the progress bar and the time played

--- src/components/Player.js
+++ src/components/Player.js
@@ -19,19 +19,47 @@ export default class Player extends React.Component{
   if (!this.props.current) {
     return null;
   }
+    // const image = this.props.current.album.images.length > 0 ? this.props.current.album.images[0].url : "";
+    console.log({content: !this.state.playing ?  "\F04B" : "\F04C"});
+    var divStyle = {
+      backgroundImage: `url(${this.props.current.album.images[0].url})`,
+      height:"100%"
+    };
+    console.log(this.props.current.artists[0].name);
+    console.log(this.props.current.album.images[0].url);
+    // {!this.state.playing?"Play" : "Pause"}
     return(
 
       <div className="playerContainer">
 
-          <h5 className="trackPlaying">{this.props.current.name}</h5>
-          {this.state.playing && "Playing"}
+      <div className="playerArtist">
+          <div className="trackImage">
+          <div className="playerImage" style={divStyle}></div>
+          </div>
+          <h6 className="trackPlaying">{this.props.current.name}</h6>
+          <h6 className="trackPlaying">{this.props.current.artists[0].name}</h6>
+          </div>
 
-          <button className="playerButton" onClick={this.props.prevTrack}> Previous Track</button>
-          <button className="playerButton" onClick={this.togglePlay}>{!this.state.playing?"Play" : "Pause"} </button>
-          <button className="playerButton" onClick={this.props.nextTrack}>Next Track </button>
+          <div className="player">
+          <div className="playerControls">
+          <div className="playerControlsButtons">
+          <a className="playerControlsButtons">
+
+
+          <i className={!this.state.playing ?  "fa fa-pause fa-2" : "fa fa-play fa-2"} onClick={this.togglePlay} >
+          </i>
+          </a>
+          </div>
+
+          <div className="playerBar">
+            <h6 className="timeBegin">1</h6>
+            <div className="progressBar"></div>
+            <h6 className="timeEnd">30</h6>
+          </div>
+        </div>
+
           <ReactPlayer onProgress= {this.handleProgress} url={this.props.current.preview_url} hidden playing={this.state.playing} />
 
-          <div className="progressBar">
           </div>
 
       </div>
